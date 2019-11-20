806902e3a26b63628661bab477646aff988461f5
Orr Shalom Dvory <dvoreader@gmail.com>
2019-11-20 17:59:33 +0200

    bug fixes.

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <stddef.h>
-void html2pdf(const char* html_txt, const char* base_uri, const char* out_uri);
+void html2pdf(const char* in_uri, const char* html_txt, const char* base_uri, const char* out_uri);
 
 int main()
 {
-    html2pdf("HELLO simple<br/>html2pdf", NULL, "file:///tmp/test.pdf");
+    html2pdf(NULL, "<div style=\"background: #ff007e;overflow: hidden;\">HELLO simple<br/>html2pdf!</div>", NULL, "file:///tmp/test1.pdf");
+    html2pdf("https://webkitgtk.org" ,NULL, NULL, "file:///tmp/test2.pdf");
+
 
     return 0;
 }
--- wkgtkprinter.c
+++ wkgtkprinter.c
@@ -3,18 +3,18 @@
 #include <webkit2/webkit2.h>
 
 
-struct PrintConfig
+struct WkGtkPrinterUserData
 {
     GtkPrintSettings * print_settings;
     WebKitPrintOperation * print_operation;
+    GMainLoop * main_loop;
 };
+#define WK_GTK_UDATA ((struct WkGtkPrinterUserData*)user_data)
 static void
 print_finished (WebKitPrintOperation *print_operation,
                gpointer              user_data)
 {
-
-    gtk_main_quit();
-
+    g_main_loop_quit (WK_GTK_UDATA->main_loop);
 }
 static void web_view_load_changed (WebKitWebView  *web_view,
                                    WebKitLoadEvent load_event,
@@ -39,11 +39,12 @@ static void web_view_load_changed (WebKitWebView  *web_view,
         break;
     case WEBKIT_LOAD_FINISHED:
         //printf("WEBKIT_LOAD_FINISHED\n");
+        printf ("printing pdf file: %s\n",gtk_print_settings_get(WK_GTK_UDATA->print_settings, GTK_PRINT_SETTINGS_OUTPUT_URI));
     {
 
 
-        GtkPrintSettings *print_settings = ((struct PrintConfig *)user_data)->print_settings;
-        WebKitPrintOperation * print_operation = ((struct PrintConfig *)user_data)->print_operation;
+        GtkPrintSettings *print_settings = WK_GTK_UDATA->print_settings;
+        WebKitPrintOperation * print_operation = WK_GTK_UDATA->print_operation;
         webkit_print_operation_print (print_operation);
     }
         break;
@@ -52,11 +53,12 @@ static void web_view_load_changed (WebKitWebView  *web_view,
 
 }
 
-void html2pdf(const char* html_txt, const char* base_uri, const char* out_uri)
+void html2pdf(const char* in_uri, const char* html_txt, const char* base_uri, const char* out_uri)
 {
-    struct PrintConfig print_config;
+
     gtk_init_check (NULL,
                     NULL);
+    struct WkGtkPrinterUserData user_data;
     GtkPrintSettings *print_settings = gtk_print_settings_new ();
     gtk_print_settings_set_printer (print_settings,
                                     "Print to File");
@@ -69,7 +71,7 @@ void html2pdf(const char* html_txt, const char* base_uri, const char* out_uri)
     gtk_print_settings_set (print_settings,
                                 GTK_PRINT_SETTINGS_OUTPUT_BASENAME,
                                 "html2pdf_test"
-                                );*/
+                                );*/ // commented out in favor of GTK_PRINT_SETTINGS_OUTPUT_URI
     gtk_print_settings_set (print_settings,
                                 GTK_PRINT_SETTINGS_OUTPUT_URI,
                                 out_uri
@@ -84,23 +86,36 @@ void html2pdf(const char* html_txt, const char* base_uri, const char* out_uri)
     gtk_print_settings_set_orientation (print_settings,
                                         GTK_PAGE_ORIENTATION_PORTRAIT);
 
-    print_config.print_settings = print_settings;
+    user_data.print_settings = print_settings;
 
 
-    WebKitWebView * web_view =  WEBKIT_WEB_VIEW( webkit_web_view_new() );
+    WebKitWebView * web_view =  WEBKIT_WEB_VIEW( webkit_web_view_new_with_context (webkit_web_context_get_default ()) );
+    g_object_ref_sink(G_OBJECT(web_view));
 
     WebKitPrintOperation * print_operation = webkit_print_operation_new (web_view);
     webkit_print_operation_set_print_settings(print_operation,print_settings);
-    g_signal_connect (print_operation, "finished", G_CALLBACK (print_finished), NULL);
-    print_config.print_operation = print_operation;
+    g_signal_connect (print_operation, "finished", G_CALLBACK (print_finished), &user_data);
+    user_data.print_operation = print_operation;
 
-    g_signal_connect (web_view, "load-changed", G_CALLBACK (web_view_load_changed), &print_config);
-    webkit_web_view_load_html (web_view, html_txt, base_uri);
+    GMainLoop * main_loop = g_main_loop_new(NULL,false);
+    user_data.main_loop = main_loop;
+    g_signal_connect (web_view, "load-changed", G_CALLBACK (web_view_load_changed), &user_data);
+    if (in_uri== NULL)
+    {
+        webkit_web_view_load_html (web_view, html_txt, base_uri);
+    }
+    else
+    {
+        webkit_web_view_load_uri(web_view, in_uri);
+    }
 
-    gtk_main();
+    g_main_loop_run (main_loop);
+
+    g_main_loop_unref (main_loop);
+
     g_object_unref( G_OBJECT(print_operation));
     g_object_unref( G_OBJECT(print_settings));
-    g_object_ref_sink(G_OBJECT(web_view));
+
     gtk_widget_destroy(GTK_WIDGET(web_view));
     g_object_unref( G_OBJECT(web_view));
 }
