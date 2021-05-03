fea10667bbc4c430a70802a2f30d82ff001579d0
Orr Shalom Dvory <dvoreader@gmail.com>
2021-05-03 23:50:34 +0300

    added wkgtkprinter.h

---
+++ wkgtkprinter.h
@@ -0,0 +1,4 @@
+void wkgtkprinter_html2pdf(const char* in_uri, const char* html_txt, const char* base_uri, const char* out_uri, const char* key_file_data, const char* default_stylesheet);
+void wkgtkprinter_gtk_mainloop_start_thread();
+void wkgtkprinter_gtk_mainloop_stop_thread();
+void wkgtkprinter_gtk_init();
